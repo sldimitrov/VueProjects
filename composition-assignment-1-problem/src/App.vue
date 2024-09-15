8ef0e99a1873d882cbffee3b003de49fc21a67aa
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-15 19:59:05 +0300

    Init a Vue project setup

---
+++ composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>My Course Goal</h2>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <h3>OUTPUT COURSE GOAL</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button>Toggle Goal</button>
+    <!-- Task 3: Manage data in three ways -->
+    <!-- => Separate refs -->
+    <!-- => Ref Object -->
+    <!-- => Reactive Object -->
+    <!-- Task 4: Also solve the assignment with the Options API -->
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const courseGoal = ref('Master every piece of VueJS Framework!');
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
