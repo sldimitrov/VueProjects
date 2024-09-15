f781423071fdb338e5ef34a46bca1ae527f1d538
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-15 20:05:41 +0300

    Finish the assignment

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -3,9 +3,9 @@
     <h2>My Course Goal</h2>
     <!-- Task 1: Output your main course goal with help of the composition API -->
     <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-    <h3>OUTPUT COURSE GOAL</h3>
+    <h3>{{ isShowed ? courseGoal : '' }}</h3>
     <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-    <button>Toggle Goal</button>
+    <button @click="toggleShow">Toggle Goal</button>
     <!-- Task 3: Manage data in three ways -->
     <!-- => Separate refs -->
     <!-- => Ref Object -->
@@ -18,6 +18,11 @@
 import {ref} from 'vue';
 
 const courseGoal = ref('Master every piece of VueJS Framework!');
+const isShowed = ref(true)
+
+const toggleShow = () => {
+  isShowed.value = !isShowed.value
+}
 </script>
 
 <style>
