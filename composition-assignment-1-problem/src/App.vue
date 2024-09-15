ee8f236afd955d6585d2dbd63c25cd1800ed52bb
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-15 20:07:26 +0300

    manage data as ref object

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -3,7 +3,7 @@
     <h2>My Course Goal</h2>
     <!-- Task 1: Output your main course goal with help of the composition API -->
     <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-    <h3>{{ isShowed ? courseGoal : '' }}</h3>
+    <h3>{{ data.isShowed ? data.courseGoal : '' }}</h3>
     <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
     <button @click="toggleShow">Toggle Goal</button>
     <!-- Task 3: Manage data in three ways -->
@@ -17,11 +17,10 @@
 <script setup>
 import {ref} from 'vue';
 
-const courseGoal = ref('Master every piece of VueJS Framework!');
-const isShowed = ref(true)
+const data = ref({courseGoal: 'Master Vue', isShowed: true, });
 
 const toggleShow = () => {
-  isShowed.value = !isShowed.value
+  data.value.isShowed = !data.value.isShowed
 }
 </script>
 
